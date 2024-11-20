3ce41d33667ba16159bf7e8825bc4c51028b7714
Victor Brown <vic.brown.s56@gmail.com>
2024-11-20 11:12:11 +0100

    Comentarios Tarea2

--- Tarea_2.py
+++ Tarea_2.py
@@ -18,19 +18,22 @@ def getdataset(images, labels, caracteres, num_pix):
     """
     import warnings
     warnings.filterwarnings("ignore")
-
+    #Iniciamos los arrays X e Y, el array X contendra 0s y
+    # tendra como dimensiones el número de imágenes x el número de pixeles
     X = np.zeros((len(images), num_pix * num_pix))
+    #El array Y tendrá una unica dimensión que será el numero de imágenes
     Y = np.empty(len(images))
 
-    images_resize = []
+    images_resize = [] #Lista donde guardaremos las imágenes con otro tamaño
 
-    for i in range(len(images)):
+    for i in range(len(images)): #Se recorren todas las imágenes y se las cambia de tamaño
         images_resize.append(resize(images[i], (num_pix, num_pix)))
-    try:
-        for i in range(len(images_resize)):
-            X[i] = images_resize[i].reshape(images_resize[i].size).tolist()
-            Y[i] = caracteres[labels[i]]
-    except ValueError:
+
+    try: #Bloque try para posibles excepciones
+        for i in range(len(images_resize)): #Se recorre images_resize
+            X[i] = images_resize[i].reshape(images_resize[i].size).tolist() #Se aplanan y se añaden a X
+            Y[i] = caracteres[labels[i]] #Se añade a Y el label correspondiente
+    except ValueError: #Posible error
         pass
 
     return X, Y
